c93e096bb00512ce927bef34f436a995c17f65f0
davide-info <83416046+davide-info@users.noreply.github.com>
2022-01-10 14:48:54 +0100

    aggiunto setHeader

--- prova.js
+++ prova.js
@@ -1,7 +1,8 @@
 const a = 4 ;
 const http = require('http');
-const server = http.createServer(() =>{
+const server = http.createServer((request,response) =>{
 console.log("I hear you thank you for a request");
+response.setHeader("QUI ");
 });
 
 server.listen(3000);
