319bc32a779e8938281ea71660174d8585154755
davide-info <83416046+davide-info@users.noreply.github.com>
2022-01-10 15:37:58 +0100

    prova req.query

--- prova.js
+++ prova.js
@@ -4,11 +4,13 @@ const http = require('http');
 const express=require('express');
 const app=express();
 app.get('/', (req,resp)=>{
+    console.log("QUERY 1" + req.query);
 resp.send("hello");
 
 });
 
 app.get("/profile" , (req,resp)=>{
+    console.log("QUERY 2" + req.query);
     resp.send("My profile");
 });
 
