85ef50046c5216a9c31678767bdd99b249b2e5b5
davide-info <83416046+davide-info@users.noreply.github.com>
2022-01-10 15:04:47 +0100

    cambiato request.end JSON.stringify

--- prova.js
+++ prova.js
@@ -4,8 +4,8 @@ const object = {name:'John', surname:"Denver"};
 
 const server = http.createServer((request,response) =>{
 console.log("I hear you thank you for a request");
-response.setHeader("Content-Type","application/json");
-response.end(object);
+response.setHeader("Content-Type","_application/json");
+response.end(JSON.stringify(object));
 });
 
 server.listen(3000);
