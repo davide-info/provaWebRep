9c405475b54719f3fb79a87171098a24a0e5709a
davide-info <83416046+davide-info@users.noreply.github.com>
2022-01-10 14:50:54 +0100

    modificato content

--- prova.js
+++ prova.js
@@ -2,7 +2,7 @@ const a = 4 ;
 const http = require('http');
 const server = http.createServer((request,response) =>{
 console.log("I hear you thank you for a request");
-response.setHeader("Conent-type ","text/html");
+response.setHeader("Content-type ","text/html");
 response.end("QUI");
 });
 
